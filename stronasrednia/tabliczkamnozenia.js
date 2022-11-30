bb263fb18231b250f1a64ba5693507d1c911a983
tokarooo <117310276+tokarooo@users.noreply.github.com>
2022-11-30 21:24:49 +0100

    Update tabliczkamnozenia.js

--- stronasrednia/tabliczkamnozenia.js
+++ stronasrednia/tabliczkamnozenia.js
@@ -3,7 +3,7 @@
         document.getElementById("m" + String(y)).innerHTML += "<p>" + y + " * " + z + " = " + (y * z) + "</p>"
     }
 }*/
-for (let y = 1; y <= 2000; y++) {
+for (let y = 1; y <= 10; y++) {
     document.getElementById("tabm").innerHTML += '<div id="m' + y + '" class="tablica" > <h2>Mnożenie przez ' + y + ':</h2 ></div > '
     for (let z = 1; z <= 9; z++) {
         document.getElementById("m" + String(y)).innerHTML += "<p>" + y + " * " + z + " = " + (y * z) + "</p>"
